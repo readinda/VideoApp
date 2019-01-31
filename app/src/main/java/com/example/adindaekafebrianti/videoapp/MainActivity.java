1803107b9ef02d1bc7f56c4bc714f2b24e7e29ec
readinda <adindaadeka48@gmail.com>
2019-01-31 09:48:27 +0700

    Initial commit

---
+++ app/src/main/java/com/example/adindaekafebrianti/videoapp/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.adindaekafebrianti.videoapp;
+
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        videoView = findViewById(R.id.videoView);
+        videoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.androidcommercial));
+        videoView.setMediaController(new MediaController(this));
+        videoView.start();
+
+
+    }
+}
